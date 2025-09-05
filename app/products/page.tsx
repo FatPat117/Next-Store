696671e27cacc75c8392c995766bdea59e98d0b6
Phat117 <ntphat110705@gmail.com>
2025-09-05 21:39:04 +0700

    NavSearch Component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,9 @@
-const ProductsPage = () => {
-        return <div>ProductsPage</div>;
+import ProductsContainer from "@/components/product/ProductsContainer";
+
+const ProductsPage = ({ searchParams }: { searchParams: { layout?: string; search?: string } }) => {
+        const layout = searchParams.layout || "grid";
+        const search = searchParams.search || "";
+        return <ProductsContainer layout={layout} search={search} />;
 };
 
 export default ProductsPage;
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,7 +1,43 @@
-import { Input } from "../ui/input";
+"use client";
+import { Input } from "@/components/ui/input";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 const NavSearch = () => {
-        return <Input name="" type="search" placeholder="search product..." className="max-w-xs dark:bg-muted"></Input>;
+        const searchParams = useSearchParams();
+        const { replace } = useRouter();
+        const [search, setSearch] = useState(searchParams.get("search")?.toString() || "");
+
+        const handleSearch = useDebouncedCallback((searchInput: string) => {
+                const params = new URLSearchParams(searchParams);
+                if (searchInput) {
+                        params.set("search", searchInput);
+                } else {
+                        params.delete("search");
+                }
+                replace(`/products?${params.toString()}`);
+        }, 500);
+
+        useEffect(() => {
+                if (!searchParams.get("search")) {
+                        setSearch("");
+                }
+        }, [searchParams.get("search")]);
+        return (
+                <Input
+                        name=""
+                        type="search"
+                        placeholder="search product..."
+                        className="max-w-xs dark:bg-muted"
+                        value={search}
+                        onChange={(e) => {
+                                setSearch(e.target.value);
+                                handleSearch(e.target.value);
+                        }}
+                ></Input>
+        );
 };
 
 export default NavSearch;
