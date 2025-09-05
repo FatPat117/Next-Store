4905cccf28f78ed5323e4aaed87d99c350da0d9c
Phat117 <ntphat110705@gmail.com>
2025-09-05 20:11:39 +0700

    Loading Container

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import LoadingContainer from "@/components/global/LoadingContainer";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
+import { Suspense } from "react";
+
 export default function HomePage() {
         return (
                 <>
                         <Hero></Hero>
-                        <FeaturedProducts></FeaturedProducts>
+                        <Suspense fallback={<LoadingContainer />}>
+                                {" "}
+                                <FeaturedProducts></FeaturedProducts>
+                        </Suspense>
                 </>
         );
 }
